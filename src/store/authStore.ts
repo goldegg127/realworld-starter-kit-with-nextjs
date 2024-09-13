07da07ef8352bc0b980b0a652733b7441284c6da
gold <goldegg127@gmail.com>
2024-09-13 15:52:39 +0900

    feat: persist global state after page refresh

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import Cookies from 'js-cookie';
 import { User } from '@/type';
 
@@ -10,28 +11,36 @@ type AuthState = {
     logout: () => void;
 };
 
-const useAuthStore = create<AuthState>(set => ({
-    token: null,
-    userInfo: null,
-    isLoggedIn: false,
-    login: (userInfo: User) => {
-        const { token } = userInfo;
+const useAuthStore = create<AuthState>()(
+    persist<AuthState>(
+        set => ({
+            token: null,
+            userInfo: null,
+            isLoggedIn: false,
+            login: (userInfo: User) => {
+                const { token } = userInfo;
 
-        Cookies.set('real-world-token', token, {
-            secure: true,
-            sameSite: 'Strict',
-            expires: 7,
-        });
+                Cookies.set('real-world-token', token, {
+                    secure: true,
+                    sameSite: 'Strict',
+                    expires: 7,
+                });
 
-        set({ token, userInfo, isLoggedIn: true });
-    },
-    logout: () => {
-        Cookies.remove('real-world-token', {
-            secure: true,
-            sameSite: 'Strict',
-        });
-        set({ token: null, isLoggedIn: false });
-    },
-}));
+                set({ token, userInfo, isLoggedIn: true });
+            },
+            logout: () => {
+                Cookies.remove('real-world-token', {
+                    secure: true,
+                    sameSite: 'Strict',
+                });
+                set({ token: null, isLoggedIn: false });
+            },
+        }),
+        {
+            name: 'auth-storage', // 로컬 스토리지에 저장할 이름
+            getStorage: () => localStorage,
+        },
+    ),
+);
 
 export default useAuthStore;
