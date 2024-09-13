49e9b9fcb80162cae48f0f3d298b617c912ac20e
gold <goldegg127@gmail.com>
2024-09-13 23:03:02 +0900

    feat: implement comment creation functionality and component

--- src/components/article/CommentEditor.tsx
+++ src/components/article/CommentEditor.tsx
@@ -1,20 +1,41 @@
-import Image from 'next/image';
+'use client';
 
-export default function CommentEditor() {
+import { useState } from 'react';
+import Image from 'next/image';
+import { postComment } from '@/api';
+import useAuthStore from '@/store/authStore';
+
+export default function CommentEditor({ slug }: { slug: string }) {
+    const [commentBody, setCommentBody] = useState('');
+    const { token, userInfo } = useAuthStore();
+    const imagePath = userInfo?.image ?? '/image/demo-avatar.png';
+
+    const handleTextarea = (event: React.FocusEvent<HTMLTextAreaElement>) => {
+        setCommentBody(event.target.value);
+    };
+
+    const handlePostComment = async () => {
+        try {
+            await postComment({ slug, commentBody, token });
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    };
+
     return (
-        <form className="card comment-form">
+        <form className="card comment-form" onSubmit={handlePostComment}>
             <div className="card-block">
-                <textarea className="form-control" placeholder="Write a comment..." rows={3}></textarea>
+                <textarea
+                    className="form-control"
+                    placeholder="Write a comment..."
+                    rows={3}
+                    onBlur={handleTextarea}></textarea>
             </div>
             <div className="card-footer">
-                <Image
-                    src="http://i.imgur.com/Qr71crq.jpg"
-                    alt=""
-                    className="comment-author-img"
-                    width={32}
-                    height={32}
-                />
-                <button className="btn btn-sm btn-primary">Post Comment</button>
+                <Image src={imagePath} alt="" className="comment-author-img" width={32} height={32} />
+                <button className="btn btn-sm btn-primary" type="submit">
+                    Post Comment
+                </button>
             </div>
         </form>
     );
