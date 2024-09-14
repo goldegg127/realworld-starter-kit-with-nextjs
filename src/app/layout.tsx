9d547babd7fb70a8d2c61c25dad3b0a325369b97
gold <goldegg127@gmail.com>
2024-09-14 12:24:47 +0900

    perf: replace <link> tags with CSS imports in modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import Head from 'next/head';
 import { Header, Footer } from '@/components/global';
+import './globals.css';
+import './external.css';
 
 export const metadata: Metadata = {
     title: 'Conduit',
@@ -10,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <head>
+            {/* <head>
                 <link
                     href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
                     rel="stylesheet"
@@ -22,7 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     type="text/css"
                 />
                 <link rel="stylesheet" href="//demo.productionready.io/main.css" />
-            </head>
+            </head> */}
             <body>
                 <Header />
                 {children}
