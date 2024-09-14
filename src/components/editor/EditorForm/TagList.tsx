4489b05dc21c72ebc92e78708630929d4f8dd35b
gold <goldegg127@gmail.com>
2024-09-14 23:45:00 +0900

    chore: add @todo comments

--- src/components/editor/EditorForm/TagList.tsx
+++ src/components/editor/EditorForm/TagList.tsx
@@ -5,6 +5,9 @@ export default function TagList({ tagList }: { tagList: string[] }) {
                 (tag, index) =>
                     tag && (
                         <span key={`${index}-${tag}`} className="tag-default tag-pill">
+                            {/**
+                             * @todo 태그 삭제 기능, PUT 메서드는 태그 수정 불가 업데이트 시 삭제기능 제거
+                             */}
                             <i className="ion-close-round"></i> {tag}
                         </span>
                     ),
