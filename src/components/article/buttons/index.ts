573d1a2df3429ef35c9c85a5ccb68083f7dab673
gold <goldegg127@gmail.com>
2024-09-14 22:23:26 +0900

    refactor: separate and export button components for modularity

---
+++ src/components/article/buttons/index.ts
@@ -0,0 +1,5 @@
+import DeleteCommentButton from './DeleteCommentButton';
+import DeleteArticleButton from './DeleteArticleButton';
+import EditArticleButton from './EditArticleButton';
+
+export { DeleteCommentButton, DeleteArticleButton, EditArticleButton };
--- src/components/article/index.ts
+++ src/components/article/index.ts
@@ -3,19 +3,6 @@ import ArticleContent from './ArticleContent';
 import ArticleActions from './ArticleActions';
 import CommentEditor from './CommentEditor';
 import CommentsList from './CommentsList';
-import DeleteCommentButton from './buttons/DeleteCommentButton';
-import DeleteArticleButton from './buttons/DeleteArticleButton';
-import EditArticleButton from './buttons/EditArticleButton';
 import ArticleGlobalStateUpdater from './ArticleGlobalStateUpdater';
 
-export {
-    ArticleBanner,
-    ArticleContent,
-    ArticleActions,
-    CommentEditor,
-    CommentsList,
-    DeleteCommentButton,
-    DeleteArticleButton,
-    EditArticleButton,
-    ArticleGlobalStateUpdater,
-};
+export { ArticleBanner, ArticleContent, ArticleActions, CommentEditor, CommentsList, ArticleGlobalStateUpdater };
