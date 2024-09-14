c7f9a9059ae975667883b515aeb0a5b07fce21df
gold <goldegg127@gmail.com>
2024-09-14 22:47:21 +0900

    fix: update imports to reflect modified component paths

--- src/components/article/ArticleActions.tsx
+++ src/components/article/ArticleActions.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { DeleteArticleButton, EditArticleButton } from '@/components/article';
+import { DeleteArticleButton, EditArticleButton } from '@/components/article/buttons';
 
 export default function ArticleActions({
     slug,
